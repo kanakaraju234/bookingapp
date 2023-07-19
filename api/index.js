a2e015162902609669798b5c1baadf56da7b82c6
vijju029 <vijayvasudev029@gmail.com>
2023-07-19 22:48:37 +0530

    all crud operations

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
-import dotenv from "dotenv";
 import express from "express";
+// @ts-ignore
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
 import hotelsRoute from "./routes/hotels.js";
 import roomsRoute from "./routes/rooms.js";
+import usersRoute from "./routes/users.js";
 
 const app = express();
 dotenv.config();
@@ -11,29 +15,23 @@ dotenv.config();
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
-    console.log("Connected to mongoDB");
+    console.log("Connected to mongoDB.");
   } catch (error) {
     throw error;
   }
 };
 
 mongoose.connection.on("disconnected", () => {
-  console.log("mongoDB disconnected");
+  console.log("mongoDB disconnected!");
 });
 
-// mongoose.connection.on("connected", ()=>{
-//     console.log("mongoDB connected");
-// });
-
-// app.get("/", (req, res) => {
-//   res.send("hello first request");
-// });
-
 //middlewares
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
-// app.use("/api/users", usersRoute);
+app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
 
@@ -50,5 +48,5 @@ app.use((err, req, res, next) => {
 
 app.listen(8000, () => {
   connect();
-  console.log("connected to BE...!");
+  console.log("Connected to backend.");
 });
